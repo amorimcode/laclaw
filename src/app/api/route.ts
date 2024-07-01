29fc866a8ffc29135e6c6885b71c91485e6a90a1
brunomaismei <bruno@maismei.com.br>
2024-07-01 19:31:47 -0300

    chore: Add API route for fetching data from different sources

---
+++ src/app/api/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+import data1 from "./fonte1.json";
+import data2 from "./fonte2.json";
+import data3 from "./fonte3.json";
+import data4 from "./fonte4.json";
+import data5 from "./fonte5.json";
+
+export async function GET(req?: Request) {
+  const url = new URL(req?.url ?? "");
+  const source = url.searchParams.get("source");
+
+  let jsonData = {};
+  switch (source) {
+    case "fonte1":
+      jsonData = data1;
+      break;
+    case "fonte2":
+      jsonData = data2;
+      break;
+    case "fonte3":
+      jsonData = data3;
+      break;
+    case "fonte4":
+      jsonData = data4;
+      break;
+    case "fonte5":
+      jsonData = data5;
+      break;
+    default:
+      jsonData = data1;
+  }
+
+  return NextResponse.json(jsonData);
+}
