c8deb3d837465156fc5589b991c6a45f90acbbe3
brunomaismei <bruno@maismei.com.br>
2024-07-01 23:07:37 -0300

    chore: Update dependencies and add @radix-ui/react-label and @radix-ui/react-radio-group for UI enhancements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,67 @@
 import { Datatable } from "@/components/Datatable";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import useTranslate from "@/hooks/useTranslate";
 
 const Home = () => {
+  const { t } = useTranslate("HOME");
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col  p-24">
+      <div className="flex space-x-4 mb-5">
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder={t("SELECTS.SUM")} />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+          </SelectContent>
+        </Select>
+
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder={t("SELECTS.VIEW_BY")} />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectContent>
+        </Select>
+
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder={t("SELECTS.DETAIL_BY")} />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+
+      <div>
+        <RadioGroup defaultValue="option-one">
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="option-one" id="option-one" />
+            <Label htmlFor="option-one">{t("RADIO.GRAPH")}</Label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="option-two" id="option-two" />
+            <Label htmlFor="option-two">{t("RADIO.TABLE")}</Label>
+          </div>
+        </RadioGroup>
+      </div>
+
       <Datatable />
     </main>
   );
