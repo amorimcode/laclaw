738c7814b62ee8f3d84eb4ed0f81e6067040a90e
brunomaismei <bruno@maismei.com.br>
2024-07-02 00:00:01 -0300

    chore: Update header logo and styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,11 +5,13 @@ const Header = () => {
   const t = useTranslations("HEADER");
 
   return (
-    <header className="bg-blue-500 p-4 shadow-md">
+    <header className="bg-zinc-950 p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-white text-2xl font-bold">
-          <Link href="/">LacLaw</Link>
-        </h1>
+        <img
+          className="w-20"
+          src="https://laclaw.com.br/wp-content/uploads/2020/06/logo-ass-contraste.svg"
+        />
+
         <nav>
           <ul className="flex space-x-4">
             <li>
